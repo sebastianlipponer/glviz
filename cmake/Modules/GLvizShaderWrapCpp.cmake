659eef737c29c97ba67ca89073534a80eca11bc6
Sebastian Lipponer <mail@sebastianlipponer.de>
2019-09-22 11:10:47 +0200

    CMake: Update to version 3.8.2

--- cmake/Modules/GLvizShaderWrapCpp.cmake
+++ cmake/Modules/GLvizShaderWrapCpp.cmake
@@ -1,19 +1,25 @@
-function(shader_wrap_cpp _OUT SHADERS)
-    foreach(SHADER ${SHADERS})
-        get_filename_component(FILE ${SHADER} NAME_WE)
-        get_filename_component(EXT ${SHADER} EXT)
+function(glviz_shader_wrap_cpp _out)
+    foreach(_shader IN LISTS ARGN)
+        get_filename_component(_filename ${_shader} NAME_WE)
 
-        string(REGEX REPLACE "\\." "_" EXT "${EXT}")
+        get_filename_component(_ext ${_shader} EXT)
+        string(REGEX REPLACE "\\." "_" _ext "${_ext}")
+
+        set(_input "${_shader}")
+        if (NOT IS_ABSOLUTE "${_input}")
+            set(_input "${CMAKE_CURRENT_SOURCE_DIR}/${_shader}")
+        endif()
+        set(_output "${CMAKE_CURRENT_BINARY_DIR}/${_filename}${_ext}.cpp")
 
         add_custom_command(
-            OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${FILE}${EXT}.cpp"
-            COMMAND embed_resource ARGS "${FILE}${EXT}" "${SHADER}"
-            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-            DEPENDS ${SHADER}
+            OUTPUT "${_output}"
+            COMMAND $<TARGET_FILE:GLviz::embed_resource> "${_filename}${_ext}" "${_input}"
+            WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
+            DEPENDS "${_input}"
         )
         
-        list(APPEND SOURCE "${CMAKE_CURRENT_BINARY_DIR}/${FILE}${EXT}.cpp")
-    endforeach(SHADER)
-
-    set(${_OUT} ${SOURCE} PARENT_SCOPE)
+        list(APPEND _shader_cpp "${_output}")
+    endforeach()
+        
+    set(${_out} ${_shader_cpp} PARENT_SCOPE)
 endfunction()
